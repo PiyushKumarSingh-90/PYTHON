93af88f261263052dee66287e7cc0f5378df2919
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2023-12-20 16:22:34 +0530

    Add files via upload

---
+++ 1_practice.py
@@ -0,0 +1,42 @@
+# a= eval(input("Enter the no: "))
+# print(a)
+
+# ls=[1,2,3,4,5]
+
+# x=ls
+# print(x,ls)
+
+# ls[2]=12
+# print(x,ls)
+
+# x=ls.append(40)
+# print(x,ls)
+
+# d={1:"Hello",2:"holla",3:"ke haal ba"}
+# print(d)
+# print(d[3])
+
+# n=int(input("enter the marks of student : "))
+
+# if(n>=70):
+#     print("distinction")
+
+# elif (n>=60 or n<=70):
+#     print("first class")
+
+# elif(n>=50 or n<=60):
+#     print("second class")
+
+# else:
+#     print("pass class")
+
+n=int(input("Enter the no. : "))
+sum=0
+for i in range(1,n+1,1):
+    sum+=i
+print(sum)
+
+
+
+
+
