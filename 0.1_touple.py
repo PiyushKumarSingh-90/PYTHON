397978cf4cae5de9f853a6db9164aa679a0b17df
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2023-12-20 16:22:02 +0530

    Add files via upload

---
+++ 0.1_touple.py
@@ -0,0 +1,16 @@
+#                 TOUPLE FUNCTION KEY
+
+
+# fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+# (green, *yellow, red) = fruits         # 1. Assign the rest of the values as a list called "yellow"
+
+# print(green)  
+# print(yellow)
+# print(red)
+
+
+# fruits = ("apple", "banana", "cherry")
+# mytuple = fruits * 2                   # 2. Multiply the fruits tuple by 2
+
+# print(mytuple)
