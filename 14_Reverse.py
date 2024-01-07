7d939d73a4e95dce0605887328a0aa17d17b715a
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2024-01-07 16:16:14 +0530

    Add files via upload

---
+++ 14_Reverse.py
@@ -0,0 +1,19 @@
+# # Input a word from the user
+# word_to_reverse = input("Enter a word: ")
+
+# # Reverse the word without using a function
+# reversed_result = word_to_reverse[::-1]
+
+# # Print the reversed word
+# print("Reversed word:", reversed_result)
+
+
+
+
+
+n=input("Enter the word u want to check : ")
+
+# l=[n]
+rev=n[::-1]
+
+print(rev)    
